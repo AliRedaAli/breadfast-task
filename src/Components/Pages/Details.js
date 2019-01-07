51a55a3514cb6af67f581eb54e4ff31a7bbf5874
Ali Reda <aliredamis@gmail.com>
2019-01-07 13:40:30 +0200

    finishing details

--- src/Components/Pages/Details.js
+++ src/Components/Pages/Details.js
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import { Button, TextField } from '@material-ui/core';
 
 const styles = {
   card: {
-    width: 450,
   },
   media: {
     height: 300,
@@ -20,7 +22,8 @@ class Details extends Component {
     super(props)
   
     this.state = {
-       product:null
+       product:null,
+       qty:1
     }
   }
   
@@ -34,22 +37,73 @@ class Details extends Component {
       })  
     }
   }
+  
+  handleChange = event => {
+    if(event.target.value > 0){
+      this.setState({ qty: event.target.value });
+    }
+  };
+
   render() {
     const { classes } = this.props;
-
+    const {product} = this.state;
     return (
       <Fragment>
-        <Card className={classes.card}>
-            <CardMedia
-              className={classes.media}
-              image={this.state.product.images}
-              title="Contemplative Reptile"
-            />
-        </Card>
+        <Grid 
+          container 
+          justify="flex-start"
+          spacing={24}>
+
+            <Grid item xs={1} md={6}>
+
+              <Card className={classes.card}>
+                  <CardMedia
+                    className={classes.media}
+                    image={product.images}
+                    title="Contemplative Reptile"
+                  />
+              </Card>
+            
+            </Grid>
+            <Grid item xs={1} md={6}>
+              <Typography component="h1" variant="h2" gutterBottom>
+                {product.name}
+              </Typography>
+              <Typography component="p" variant="p" gutterBottom>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam placerat leo ut velit convallis lobortis.
+              </Typography>
+              <Typography component="h5" variant="h5" gutterBottom>
+                Price: {Number(product.price).toFixed(2).toLocaleString()} EGP
+              </Typography>
+              <Grid container direction='row' alignItems='center'>
+                <Grid item xs='6'>
+                  <TextField
+                    id="outlined-number"
+                    label="QTY"
+                    value={this.state.qty}
+                    onChange={this.handleChange}
+                    type="number"
+                    className={classes.textField}
+                    InputLabelProps={{
+                      shrink: true,
+                    }}
+                    margin="normal"
+                    variant="outlined"
+                  />
+                </Grid>
+                <Grid item xs='6'>
+                  <Button variant="contained" color='primary'>
+                    Add To Cart
+                  </Button>
+              </Grid>
+              </Grid>
+            </Grid>
+        </Grid>
       </Fragment>
     )
   }
 }
+
 Details.propTypes = {
   classes: PropTypes.object.isRequired,
 };
